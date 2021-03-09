4668c3b629053a75b4466d5406556cbee5873875
Sergi <sjustinianoclaramunt@gmail.com>
2021-03-09 17:19:45 +0100

    checkEndGame-deployed

--- scripts/app.js
+++ scripts/app.js
@@ -12,17 +12,94 @@ $(document).ready(function(){
         currentPlayer = currentPlayer === cross ? circle : cross;
 
         $('.turn-indicator').toggle();
-        //checkEndGame();
+        checkEndGame();
+        if(STATUS.winner){
+            alert(STATUS.winner)
+        }
+        
     });
 })
 
-// function checkEndGame () {
-//     for
-//     for
-//     if
-//     if
+function checkEndGame () {
+    //Rows
+    //Horitzontal
+    for(row of [1,2,3]){
+        var count = 0;
+        for(col of [1,2,3]){
+            if($(`div.cell[data-row="${row}"][data-col="${col}"]`).hasClass("cross")){
+                count ++;
+            }
+            if($(`div.cell[data-row="${row}"][data-col="${col}"]`).hasClass("circle")){
+                count --;
+            }
+       }
+       if(count == 3){
+           STATUS.winner = "cross"
 
-// }
+       }
+       if(count == -3){
+           STATUS.winner ="circle"
+       }
+       console.log("fila", row, "vale", count)
+    }
+    //Cols
+    for(col of [1,2,3]){
+        var count = 0;
+        for(row of [1,2,3]){
+            if($(`div.cell[data-row="${row}"][data-col="${col}"]`).hasClass("cross")){
+                count ++;
+            }
+            if($(`div.cell[data-row="${row}"][data-col="${col}"]`).hasClass("circle")){
+                count --;
+            }
+       }
+       if(count == 3){
+           STATUS.winner = "cross"
+
+       }
+       if(count == -3){
+           STATUS.winner ="circle"
+       }
+       console.log(count)
+    }
+
+    //Principal diagonal
+    var count = 0;
+    for(i of [1,2,3]){
+        
+        if($(`div.cell[data-row="${i}"][data-col="${i}"]`).hasClass("cross")){
+            count ++;
+        }
+        if($(`div.cell[data-row="${i}"][data-col="${i}"]`).hasClass("circle")){
+            count --;
+        }
+    }
+    if(count == 3){
+        STATUS.winner = "cross"
+
+    }
+    if(count == -3){
+        STATUS.winner ="circle"
+    }
+    count = 0;
+    //Secondary diagonal
+    for(i of [1,2,3]){
+        
+        if($(`div.cell[data-row="${i}"][data-col="${4-i}"]`).hasClass("cross")){
+            count ++;
+        }
+        if($(`div.cell[data-row="${i}"][data-col="${4-i}"]`).hasClass("circle")){
+            count --;
+        }
+    }
+    if(count == 3){
+        STATUS.winner = "cross"
+
+    }
+    if(count == -3){
+        STATUS.winner ="circle"
+    }
+}
 
 
 
